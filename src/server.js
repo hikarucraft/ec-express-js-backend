5fa9ee248b20cd5ef3970088a562b1af75f188ec
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-06-18 03:17:48 +0900

    Add script commands to start server and created endpoint.

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
-console.log('hoge');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('This is endpoint. (server.js)');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000.');
+});
