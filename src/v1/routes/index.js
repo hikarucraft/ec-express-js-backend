095cf85e2e5712b767137e0823a52b0304ab7317
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-06-18 03:45:49 +0900

    Create simple route logic

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -1,1 +1,20 @@
-console.log('v1/routes/index.js');
+import express from 'express';
+
+const app = express();
+
+app.get('/v1', (req, res) => {
+  try {
+    res.status.json({
+      status: 'success',
+      data: [],
+      message: 'Success to request /v1 endpoint.',
+    });
+  } catch (err) {
+    res.status.json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  }
+});
+
+export default app;
