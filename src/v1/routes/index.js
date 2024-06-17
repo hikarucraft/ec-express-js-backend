0c2b14efedea02c9554f996b5b7876b5153f4aed
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-06-18 05:02:02 +0900

    Fix response method

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -4,13 +4,13 @@ const app = express();
 
 app.get('/v1', (req, res) => {
   try {
-    res.status.json({
+    res.status(200).json({
       status: 'success',
       data: [],
       message: 'Success to request /v1 endpoint.',
     });
   } catch (err) {
-    res.status.json({
+    res.status(500).json({
       status: 'error',
       message: 'Internal Server Error',
     });
